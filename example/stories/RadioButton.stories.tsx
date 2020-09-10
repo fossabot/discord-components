46e1afd8b49c725eeb4b0ab3d018e59b514f6456
piko <parangee9706@gmail.com>
2020-09-10 15:46:22 +0900

    [추가] 다크모드 추가

--- example/stories/RadioButton.stories.tsx
+++ example/stories/RadioButton.stories.tsx
@@ -37,3 +37,40 @@ export const Default = () => (
     />
   </div>
 )
+
+export const Dark = () => (
+  <div className='dark-theme'>
+    <RadioSelect
+      items={[
+        {
+          title: '나를 안전하게 보호하기',
+          description: '모든 사용자의 개인 메시지를 스캔하기.',
+          value: 'sel1',
+          color: 'green',
+          default: true
+        },
+        {
+          title: '착한 친구만 있어요',
+          description: '친구가 아닌 모든 사람의 메시지를 스캔하기.',
+          value: 'sel2',
+          color: 'yellow'
+        },
+        {
+          title: '스캔하지 않기',
+          description: '개인 메시지에서는 유해 콘텐츠를 스캔하지 않아요.',
+          value: 'sel3',
+          color: 'red'
+        },
+        {
+          title: '대충 보라색',
+          value: 'purple',
+          color: 'purple'
+        }
+      ]}
+    />
+  </div>
+)
+
+Dark.parameters = {
+  backgrounds: { default: 'dark' }
+}
